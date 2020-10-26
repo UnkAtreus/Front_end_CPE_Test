278fcbec3a57338228a84c29f9dfe685acdd80a8
UnkAtreus <nom_noon00548@hotmail.com>
2020-10-26 10:56:47 +0700

    First Commit

---
+++ src/containers/HomeContainer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  HomeStyled
+} from './styled'
+
+const HomeContainer = ({ match, ...props }) => {
+  return (
+    <React.Fragment>
+      <HomeStyled>
+      </HomeStyled>
+    </React.Fragment>
+  );
+};
+
+export default HomeContainer;
